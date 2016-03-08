1813aaf624404579c7e52b15bf190374a3ead1b2
Julie Lee <julielee@julie-no-macbook-pro.local>
2016-03-08 20:04:49 +0900

    Initial Commit

---
+++ FilterCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FilterCamera
+//
+//  Created by Julie Lee on 2016/03/08.
+//  Copyright © 2016年 JulieLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
